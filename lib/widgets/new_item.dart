424f12c0aa166e184e764743250c91a7b75673ed
Diego Auza <diegoauza@Diegos-MacBook-Pro.local>
2024-01-23 12:41:48 -0500

    adding new items

---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,13 @@
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
